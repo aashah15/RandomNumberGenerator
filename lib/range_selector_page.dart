265c031e5fb85104f88e91133f705b5a963e87a8
Ashraf Shah <ashrafashah@hotmail.com>
2022-07-02 18:17:42 -0600

    Basic UI completed

---
+++ lib/range_selector_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  const RangeSelectorPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<RangeSelectorPage> createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
