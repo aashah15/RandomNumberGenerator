b9b260c01387561e6796e4c07a3da80516881525
Ashraf Shah <ashrafashah@hotmail.com>
2022-07-02 19:40:46 -0600

    Basic Input Form UI created

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -12,6 +12,50 @@ class RangeSelectorPage extends StatefulWidget {
 class _RangeSelectorPageState extends State<RangeSelectorPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Range'),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Minimum',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Maximum',
+                ),
+                keyboardType: TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_forward),
+        onPressed: () {
+          // TODO: Validate the Form
+          // TODO: Navigate to the generator page
+        },
+      ),
+    );
   }
 }
