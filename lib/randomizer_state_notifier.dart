96c8d5a63eae05ef188413c740d19b00544c2b4b
Ashraf Shah <ashrafashah@hotmail.com>
2022-07-14 20:31:35 -0600

    Deleting RandomizerChangeNotifier class

--- lib/randomizer_state_notifier.dart
+++ lib/randomizer_state_notifier.dart
@@ -34,17 +34,3 @@ class RandomizerStateNotifier extends StateNotifier<RandomizerState> {
     state = state.copyWith(max: value);
   }
 }
-
-class RandomizerChangeNotifier extends ChangeNotifier {
-  final _randomGenerator = Random();
-
-  int? _generatedNumber;
-  int? get generatedNumber => _generatedNumber;
-
-  int min = 0, max = 0;
-
-  void generateRandomNumber() {
-    _generatedNumber = min + _randomGenerator.nextInt(max + 1 - min);
-    notifyListeners();
-  }
-}
