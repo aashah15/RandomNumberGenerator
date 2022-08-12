bee15294c7322856f38185337f525c83da6ed69c
Ashraf Shah <ashrafashah@hotmail.com>
2022-08-11 18:30:58 -0600

    Input Form completed

--- lib/range_selector_page.dart
+++ lib/range_selector_page.dart
@@ -12,6 +12,50 @@ class RangeSelectorPage extends StatefulWidget {
 class _RangeSelectorPageState extends State<RangeSelectorPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Range'),
+      ),
+      body: Form(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Minimum',
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Maximum',
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: false,
+                  signed: true,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_forward),
+        onPressed: () {
+          // ToDO: validate the Form
+          // ToDo: navigate to the generator page
+        },
+      ),
+    );
   }
 }
