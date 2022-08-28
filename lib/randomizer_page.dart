a14977b17a359fd4f4d663dcabe1345f83360c0d
Ashraf Shah <ashrafashah@hotmail.com>
2022-08-27 18:45:01 -0600

    Updated

--- lib/randomizer_page.dart
+++ lib/randomizer_page.dart
@@ -1,11 +1,9 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:random_number_generator/randomizer_change_notifier.dart';
 
 class RandomizerPage extends StatelessWidget {
-  RandomizerPage({
+  const RandomizerPage({
     Key? key,
   }) : super(key: key);
 
