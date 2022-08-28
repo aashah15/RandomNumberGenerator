c3346106649cbf9e8ba0b93a657e92020678a7b7
Ashraf Shah <ashrafashah@hotmail.com>
2022-08-28 10:52:48 -0600

    Updated

--- lib/randomizer_state_notifier.dart
+++ lib/randomizer_state_notifier.dart
@@ -1,6 +1,5 @@
 import 'dart:math';
 
-import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
